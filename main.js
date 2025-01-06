0ad4fa36fa8ba5d708a4b35041f7c6631124009a
Majd El Madani <106382063+ItsZeus1@users.noreply.github.com>
2025-01-06 09:11:56 +0300

    VirtualRepos: update main.js

--- main.js
+++ main.js
@@ -10,4 +10,21 @@ document.addEventListener('DOMContentLoaded', (event) => {
       });
     });
   });
+
+
+  let currentPage = 2; 
+  const totalPages = 3; 
+  function goToPage(page) 
+  { currentPage = page; updatePagination();
+
+   } 
+  function updatePagination() { 
+  const pages = document.querySelectorAll('.pagination .page-item');
+   pages.forEach(page => { page.classList.remove('active');
+     if (page.querySelector('.page-link').getAttribute('onclick') === `goToPage(${currentPage})`) {
+       page.classList.add('active'); } 
+      }); 
+      document.getElementById('prevButton').classList.toggle('disabled', currentPage === 1); 
+      document.getElementById('nextButton').classList.toggle('disabled', currentPage === totalPages); 
+    } 
   
