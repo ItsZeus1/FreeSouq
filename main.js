4df030e581d05e4b041a9d976d9434aa764e78f8
Majd El Madani <106382063+ItsZeus1@users.noreply.github.com>
2025-01-10 09:13:42 +0300

    VirtualRepos: update main.js

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const pagination = document.getElementById('pagination');
-    const pageItems = pagination.querySelectorAll('.page-item');
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     const pagination = document.getElementById('pagination');
+//     const pageItems = pagination.querySelectorAll('.page-item');
   
-    pageItems.forEach(item => {
-      item.addEventListener('click', function(event) {
-        event.preventDefault(); 
-        pageItems.forEach(item => item.classList.remove('active'));
-        this.classList.add('active');
-      });
-    });
-  });
+//     pageItems.forEach(item => {
+//       item.addEventListener('click', function(event) {
+//         event.preventDefault(); 
+//         pageItems.forEach(item => item.classList.remove('active'));
+//         this.classList.add('active');
+//       });
+//     });
+//   });
 
 
   // let currentPage = 1; 
