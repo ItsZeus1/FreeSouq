3294f0d62c68ba1f84bd555fa48dc54458b2a6a5
Majd El Madani <106382063+ItsZeus1@users.noreply.github.com>
2025-01-10 08:29:28 +0300

    VirtualRepos: update main.js

--- main.js
+++ main.js
@@ -29,3 +29,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
       
     } 
     updatePagination();
+
+
+
+    document.addEventListener("DOMContentLoaded", function() { // Get the current page URL var currentPage = window.location.pathname; // Determine which button to activate based on the current page if (currentPage.includes("index.html")) { document.querySelector(".page-item.active").classList.remove("active"); document.querySelector(".page-item:nth-child(2)").classList.add("active"); } else if (currentPage.includes("page2.html")) { document.querySelector(".page-item.active").classList.remove("active"); document.querySelector(".page-item:nth-child(3)").classList.add("active"); } else if (currentPage.includes("page3.html")) { document.querySelector(".page-item.active").classList.remove("active"); document.querySelector(".page-item:nth-child(4)").classList.add("active"); } 
