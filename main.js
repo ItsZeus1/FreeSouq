61e2bb2c2d99cced6f4e92cde115b62fc941bf10
Majd El Madani <106382063+ItsZeus1@users.noreply.github.com>
2025-01-15 10:38:31 +0300

    VirtualRepos: update main.js

--- main.js
+++ main.js
@@ -28,15 +28,30 @@
 
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    var icon = document.getElementById('#togglePassword');
-    icon.addEventListener('click', function() {
-        if (icon.src.includes('eyeclose24.svg')) {
-            icon.src = 'eyeopen24.svg';
-        } else {
-            icon.src = 'eyeclose24.svg';
-        }
+// document.addEventListener('DOMContentLoaded', function() {
+//     var icon = document.getElementById('#togglePassword');
+//     icon.addEventListener('click', function() {
+//         if (icon.src.includes('eyeclose24.svg')) {
+//             icon.src = 'eyeopen24.svg';
+//         } else {
+//             icon.src = 'eyeclose24.svg';
+//         }
+//     });
+// });
+
+
+document.addEventListener('DOMContentLoaded', function() { 
+  var icon = document.querySelector('#toggleIcon'); 
+  icon.addEventListener('click', function() { 
+    console.log('Icon clicked'); // Log when the icon is clicked 
+    var currentSrc = icon.getAttribute('src'); 
+    console.log('Current src:', currentSrc); // Log current src 
+    if (currentSrc === 'eyeopen24.svg') { 
+      icon.setAttribute('src', 'eyeclose24.svg'); 
+    } 
+    else { icon.setAttribute('src', 'eyeopen24.svg'); 
+
+    } 
+    console.log('New src:', icon.getAttribute('src')); 
     });
-});
-
-
+  });
