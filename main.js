5ba98e58cae6278ca0256119a593b12981d350fe
nezardak <nezarpg06@gmail.com>
2025-03-20 18:14:20 +0300

    fghb

--- main.js
+++ main.js
@@ -56,4 +56,15 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
  
+  let add = document.querySelectorAll('#add');
+  let count = document.querySelectorAll('#count');
+  add.onclick = function()
+  {
+   count.innertext ='1'
+   count.innertext ='2'
+   count.innertext ='3'
+   count.innertext ='4'
+   count.innertext ='5'
+
+  }
   
